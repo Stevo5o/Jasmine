27a28d5c75573f5f68c33f13eab0494be69d805e
Stevo5o <stevo.joc@gmail.com>
2015-03-15 14:26:02 +0000

    Conditional execution

--- public_html/script.js
+++ public_html/script.js
@@ -8,35 +8,14 @@
  */
 
 // immediately invoked anonymous function
-(function() {
+(function () {
 
-	// Eloquent JS
-	var total = 0,
-		count = 1;
-	while (count <= 10) {
-		total += count;
-		count++;
-	}
-	console.log('Total:' + total);
-
-	function fac(n) {
-		if (n === 0)
-			return 1;
-		else {
-			return fac(n - 1) * n;
-		}
-	}
-	var result = fac(8);
-	console.log(typeof result + ": " + result);
-	console.log(result > total);
-	console.log(result < total);
-
-	// campare strings 
-	console.log("Aardvark" < "Zoroaster");
-
-	// true middle val
-	console.log(true ? 1 : 2);
-	// false right val
-	console.log(false ? 1 : 2);
+    // Eloquent JS
+    var theNumber = Number(prompt("Pick a number", ""));
+    if (!isNaN(theNumber))
+        alert("Your number is the square root of " +
+                theNumber * theNumber);
+    else
+        alert("Hey. Why didn't you give me a number?");
 
 }()); // end anon func
