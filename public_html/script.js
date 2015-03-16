f2fa0c5883a3a6f213b32911e5da02887ae28218
Stevo5o <stevo.joc@gmail.com>
2015-03-16 18:12:38 +0000

    Functions Defining a function, Parameters and scopes  && Nested scope

--- public_html/script.js
+++ public_html/script.js
@@ -13,7 +13,7 @@
 
 var square = function(x) {
 	return x * x;
-}
+};
 
 console.log(square(5));
 
@@ -26,9 +26,53 @@ makeNoise();
 var power = function(base, exponent) {
 	var result = 1;
 	for (var count = 0; count < exponent; count++) {
-		result *= base;		
+		result *= base;
 	}
 	return result;
 };
 
 console.log(power(2, 10));
+
+// parameters ans scopes
+var x = "outside";
+
+var f1 = function() {
+	var x = "inside f1";
+};
+f1();
+console.log(x);
+// outside
+
+var f2 = function() {
+	x = "inside f2";
+};
+f2();
+console.log(x);
+// inside f2
+
+// nested scope
+var landscape = function() {
+	var result = "";
+	var flat = function(size) {
+		for (var count = 0; count < size; count++) {
+			result += "_";
+		}
+	};
+	var mountain = function(size) {
+		result += "/";
+		for (var count = 0; count < size; count++) {
+			result += "'";
+		}
+		result += "\\";
+	};
+
+	flat(3);
+	mountain(4);
+	flat(6);
+	mountain(1);
+	flat(1);
+	return result;
+};
+
+console.log(landscape());
+// ___/''''\______/'\_
