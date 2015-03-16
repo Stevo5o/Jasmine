cd4f16fe26b28102cf2562e6f8459fee915c4c3c
Stevo5o <stevo.joc@gmail.com>
2015-03-16 15:49:43 +0000

    Beginning Chapter 3

--- public_html/fizzBuzzTest.js
+++ public_html/fizzBuzzTest.js
@@ -2,13 +2,13 @@
 
 // FizzBuzz
 for (var i = 1; i <= 100; i++) {
-	if(i % 15 == 0) {
+	if(i % 15 === 0) {
 		console.log("FizzBuzz");
 	}
-	else if(i % 5 == 0) {
+	else if(i % 5 === 0) {
 		console.log("Buzz");
 	}
-	else if(i % 3 == 0) {
+	else if(i % 3 === 0) {
 		console.log("Buzz");
 	}
 	
@@ -20,11 +20,13 @@ for (var i = 1; i <= 100; i++) {
 // FizzBuzz
 for (var n = 1; n <=100; n++) {
 	var output = "";
-	if(n % 3 == 0) {
+	if(n % 3 === 0) {
 		output += "Fizz"
 	}
-	if(n % 5 == 0 ) {
+	if(n % 5 === 0 ) {
 		output += "Buzz"
 	}
 	console.log(output || n)
 }
+
+console.log(24 === '24'); // false
