455f2d2d990485c683efcb1bacf46953f95cb1dc
Stevo5o <stevo.joc@gmail.com>
2015-03-17 15:26:33 +0000

    Functions Recursion Closure Optional arguments

--- public_html/script.js
+++ public_html/script.js
@@ -33,7 +33,7 @@ var power = function(base, exponent) {
 
 console.log(power(2, 10));
 
-// parameters ans scopes
+// parameters and scopes
 var x = "outside";
 
 var f1 = function() {
@@ -76,3 +76,76 @@ var landscape = function() {
 
 console.log(landscape());
 // ___/''''\______/'\_
+
+// optional arguments
+(function() {
+	function power(base, exponent) {
+		if (exponent == undefined) {
+			exponent = 2;
+		}
+		var result = 1;
+		for (var count = 0; count < exponent; count++) {
+			result *= base;
+		}
+		return result;
+	};
+	console.log(power(4));
+	// 16
+	console.log(power(4, 3));
+	// 64
+}());
+
+// console.log function outputs all of the values it is given
+console.log("R", 2, "D", 2);
+
+// closure 
+function wrapValue(n) {
+	var localVariable = n;
+	return function() {
+		return localVariable;
+	};
+};
+
+var wrap1 = wrapValue(1);
+var wrap2 = wrapValue(2);
+console.log(wrap1());
+// 1
+console.log(wrap2());
+// 2
+
+function multiplier(factor) {
+	return function(number) {
+		return number * factor;
+	};
+};
+
+var twice = multiplier(2);
+console.log(twice(5));
+// 10
+
+// recursion
+function power(base, exponent) {
+	if (exponent == 0) {
+		return 1;
+	} else {
+		return base * power(base, exponent - 1);
+	}
+};
+
+console.log(power(2, 3));
+// 8
+
+function findSolution(target) {
+	function find(start, history) {
+		if (start == target) {
+			return history;
+		} else if (start > target) {
+			return null;
+		} else {
+			return find(start + 5, "(" + history + " + 5)") ||
+				find(start * 3, "(" + history + " * 3)");
+		}
+	};
+	return find(1, "1");
+};
+console.log(findSolution(24));
