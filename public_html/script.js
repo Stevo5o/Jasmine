d2139e38be5241c25e5df61272446748116db84b
Stevo5o <stevo.joc@gmail.com>
2015-03-18 18:56:35 +0000

    Example of object

--- public_html/script.js
+++ public_html/script.js
@@ -17,7 +17,7 @@
 	var listOfNumbers = [2, 3, 5, 7, 11];
 	console.log(listOfNumbers[1]);
 	// 3
-	console.log(listOfNumbers[1 -1]);
+	console.log(listOfNumbers[1 - 1]);
 	// 2
 
 	// methods
@@ -39,5 +39,43 @@
 	console.log(mack);
 	// [ 'Mack', 'the' ]
 
-}());
+	// objects
+	var day1 = {
+		squirrel: false,
+		events: ["work", "touched tree", "pizza", "running", "coding"]
+	};
+	console.log(day1.squirrel);
+	// 	false	
+	console.log(day1.wolf);
+	// undefined	
+	day1.wolf = false;
+	console.log(day1.wolf);
+	// false
 
+	// delete operator - not common
+	var anObject = {
+		left: 1,
+		right: 2
+	};
+	console.log(anObject.left);
+	// 1
+	delete anObject.left;
+	console.log(anObject.left);
+	// undefined
+	console.log("left" in anObject);
+	// false
+	console.log("right" in anObject);
+	// true
+        
+        // object example
+	var journal = [{
+		events: ["work", "touched tree", "pizza", "swimming", "learning"],
+		squirrel: false
+	}, {
+		events: ["work", "ice cream", "cabbage", "seafood lasagna", "touched tree", "brushed"],
+		squirrel: false
+	}, {
+		events: ["weekend", "cycling", "break", "peanuts", "beer"],
+		squirrel: true
+	}];
+}());
