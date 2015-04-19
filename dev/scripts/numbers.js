1e813bb7a0a9faacda73d452d51d039366898ef8
Stevo5o <stevo.joc@gmail.com>
2015-04-19 14:41:09 +0100

    Set-up & tear-down

--- dev/scripts/numbers.js
+++ dev/scripts/numbers.js
@@ -9,6 +9,9 @@ define(function() {
 		total = 0;
 
 		operands.forEach(function(value) {
+			if(typeof value === 'string') {
+				value = parseInt(value, 10) || 0;
+			}			
 			total += value;
 		});
 		return total;	
--- test/spec/numbers.js
+++ test/spec/numbers.js
@@ -1,23 +1,39 @@
-/*globals define, describe*/
+/*globals define, describe, it, beforeEach*/
 define(['numbers'], function(numbers) {
-	'use strict';
+    'use strict';
 
-	describe('The numbers module', function() {
-		describe('The add method', function() {
-			it('should accept one or more numberical arguments and return the sum of them', function() {
-				// triple AAA test pattern
-				// arrange 
-				var output,
-					input1 = 1,
-					input2 = 2;
+    describe('The numbers module', function() {
+        describe('The add method', function() {
+            var output;
 
-				// act
-				output = numbers.add(input1, input2);
+            beforeEach(function() {
+                this.numberInput1 = 1;
+                this.numberInput2 = 2;
+                this.stringInput1 = '1';
+                this.stringInput2 = 'oops';
 
-				// assert
-				expect(output).toEqual(3);
-				expect(output).not.toEqual(4);
-			});
-		});
-	});
+            });
+
+            it('should accept one or more numberical arguments and return the sum of them', function() {
+                // act
+                output = numbers.add(this.numberInput1, this.numberInput2);
+
+                // assert
+                expect(output).toEqual(3);
+                expect(output).not.toEqual(4);
+            });
+
+            it('should try to parse an integer when a string is passed to the method', function() {
+                output = numbers.add(this.numberInput1, this.numberInput1);
+
+                expect(output).toEqual(2);
+            });
+
+            it('should ignore the agrument if it is not a parseable string', function() {
+                output = numbers.add(this.numberInput1, this.stringInput2);
+
+                expect(output).toEqual(1);
+            })
+        });
+    });
 });
